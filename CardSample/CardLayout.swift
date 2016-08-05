10ed73b22a220865a44f1402445a0405def62f99
Emiaostein <Emiaostein@Emiaostein.lan>
2016-08-05 18:47:10 +0800

    Initial Commit.

---
+++ CardSample/CardLayout.swift
@@ -0,0 +1,13 @@
+//
+//  CardLayout.swift
+//  CardSample
+//
+//  Created by Emiaostein on 8/5/16.
+//  Copyright © 2016 Emiaostein. All rights reserved.
+//
+
+import UIKit
+
+class CardLayout: UICollectionViewLayout {
+
+}
